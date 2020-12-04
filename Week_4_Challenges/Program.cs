e4311ce59a4c65f14cc0a83a7b3cec21ae8d061f
danisthomas <danielesthomas6@gmail.com>
2020-12-03 19:36:58 -0500

    adding week 4 challenges

---
+++ Week_4_Challenges/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_Challenges
+{
+    class Program
+    {
+        public void Main(string[] args)
+        {
+            var cat = new Cat();
+            cat.Breed = "Bengal";
+            cat.Carnivore = true;
+            cat.Color = "Snow Leopard";
+            cat.Name = "Taz";
+            cat.Size = "Small";
+            cat.Species= "Feline";
+            cat.Nocturnal = true;
+            cat.Owner = "Daniele Thomas";
+
+        }
+    }
+}
---
+++ Week_4_Challenges/Week4Challenges.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_Challenges
+{
+    abstract class Animal
+    {
+        
+
+        public bool Nocturnal { get; set; }
+
+        public bool Carnivore { get; set; }
+
+        public string Species { get; set; }
+
+        public string Color { get; set; }
+
+        public Animal () { }
+
+        public Animal (string color, bool nocturnal, bool carnivore, string species)
+        {
+            color = Color;
+            nocturnal = Nocturnal;
+            carnivore = Carnivore;
+            species = Species;
+        }
+
+
+    }
+
+    class Pet : Animal
+    {
+        public Pet() { }
+        public string Breed { get; set; }
+        public string Size { get; set; }
+
+
+    }
+    
+
+    class Cat : Pet
+    {
+        public string Name { get; set; }
+        public string Owner { get; set; }
+  
+    }
+}
